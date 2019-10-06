cf46676c8b153dce233938e837f24b972c7ad5d4
TNsaturday <tnsaturday@gmail.com>
2019-10-06 19:42:05 +0300

    Fix admin component constructor, add flights for current dates

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Flight } from '../flights/flight.model';
 import { flights } from '../flights/flight.data';
 import { Router } from '@angular/router';
+import { FlightService } from '../flights/flight.service';
 
 @Component({
   selector: 'app-admin',
@@ -15,14 +16,21 @@ export class AdminComponent implements OnInit {
   displayedColumns: string[] = [
     'Id', 'Company', 'Name', 'From', 'From Code', 'To', 'To Code',
     'Departure Date/Time', 'Arrival Date/Time', 'Price', 'Duration', 'Actions'];
-  constructor(private router: Router) {
-    this.flightStorage = window.localStorage;
-    this.flights = JSON.parse(this.flightStorage.flights);
+  constructor(private flightService: FlightService, private router: Router) {
   }
 
+
   ngOnInit() {
-    this.flightsString = JSON.stringify(flights);
+    // this.flightsString = JSON.stringify(flights);
+    // this.flightStorage.setItem('flights', this.flightsString);
+    this.getFlights();
+  }
+
+  getFlights() {
+    this.flightStorage = window.localStorage;
+    this.flightsString = JSON.stringify(this.flightService.getFlights());
     this.flightStorage.setItem('flights', this.flightsString);
+    this.flights = JSON.parse(this.flightStorage.flights);
   }
 
   editFlight(flight) {
